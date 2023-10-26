b59c0e4e3f98812cc2137a6820e8c70575e5b735
Atilio Passoni <atiliopassoni@gmail.com>
2023-10-26 15:44:44 -0300

    Ajustes consulta API JUJUY

--- js/api.js
+++ js/api.js
@@ -1,48 +1,31 @@
-
-// Reemplaza 'TU_API_KEY' con tu clave API de OpenWeatherMap
-//const apiKey = '7850e667612bca42d6fd22c055ee27c8';
+const apiKey = "7850e667612bca42d6fd22c055ee27c8";
 //const city = 'Jujuy';
+const city2 = "Ushuaia";
+//const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-// URL de la API de OpenWeatherMap
-//const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function consultarTemperatura(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const temperatura = data.main.temp;
 
-// Realizar la solicitud a la API
-//fetch(apiUrl)
-//    .then(response => response.json())
-//    .then(data => {
-        // Actualizar la información en la página
-//        document.getElementById('temperature').textContent = data.main.temp;
-//        document.getElementById('weather').textContent = data.weather[0].description;
-//    })
-//    .catch(error => console.error('Error:', error));
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Tu código JavaScript aquí
-
-
-
-
-const apiKey = '7850e667612bca42d6fd22c055ee27c8';
-const city = 'Jujuy';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
-function consultarTemperatura() {
-    fetch(url)  
-        .then(response => response.json())
-        .then(data => {
-            const temperatura = data.main.temp;
-            document.getElementById('temperatura').textContent = `${temperatura} °C`;
-        })
-        .catch(error => {
-            console.error('Error al consultar la temperatura:', error);
-        });
+      document.getElementById("temperatura").textContent = `${temperatura} °C`;
+    })
+    .catch((error) => {
+      console.error("Error al consultar la temperatura:", error);
+    });
 }
 
-// Consultar la temperatura inicialmente
-consultarTemperatura();
+
 
 // Programar actualización cada 1 minuto (60000 ms)
 setInterval(consultarTemperatura, 60000);
 
-});
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const ciudad = urlParams.get("ciudad")
+    consultarTemperatura(ciudad);
+
+})
