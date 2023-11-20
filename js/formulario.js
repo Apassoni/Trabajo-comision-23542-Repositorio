47273880abb618fca1d19ee22adb10aaf26898d6
Atilio Passoni <atiliopassoni@gmail.com>
2023-11-20 20:24:35 -0300

    Validar formulario js

---
+++ js/formulario.js
@@ -0,0 +1,36 @@
+var nombre = document.getElementById("fname");
+var apellido = document.getElementById("lname");
+var correo = document.getElementById("email");
+var telefono = document.getElementById("phone");
+var mensaje = document.getElementById("message");
+var error = document.getElementById("error");
+error.style.color = "red";
+
+
+var form = document.getElementById("formulariojs");
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  var mensajesError = [];
+
+  if (nombre.value === null || nombre.value === "") {
+    mensajesError.push("Ingresa un nombre");
+  }
+
+  if (apellido.value === null || apellido.value === "") {
+    mensajesError.push("Ingresa un apellido");
+  }
+
+  if (correo.value === null || correo.value === "") {
+    mensajesError.push("Ingresa un correo valido");
+  }
+
+  if (telefono.value === null || telefono.value === "") {
+    mensajesError.push("Ingresa un telefono de contacto");
+  }
+
+  if (mensaje.value === null || nombre.value === "") {
+    mensajesError.push("Completa el campo mensaje");
+  }
+
+  error.innerHTML = mensajesError.join(", ");
+});
